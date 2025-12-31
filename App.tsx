9ef80eafdbf94f88dadde11c9bb93617d62a4c5e
Mansoor Ali <mansoorsahito601@gmail.com>
2025-12-31 12:21:56 +0500

    Styles pro

--- App.tsx
+++ App.tsx
@@ -1,45 +1,36 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { Alert, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+const App = () => {
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Title</Text>
+      <Text style={styles.textheading} numberOfLines={4}>
+        Lorem Ipsum dolor sit
+        <Text
+          style={styles.textlink}
+          onPress={() => Alert.alert('Button Pressed ')}
+        >
+          Press Here
+        </Text>
+        amet, consectetur adisipiscing elit, sed do eiusmod tempor incididunt ut
+        lahore labore et dolore magna aliqua.
+      </Text>
+    </SafeAreaView>
   );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+};
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: '#fff' },
+  text: {
+    textAlign: 'center',
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: '#1b241f',
+  },
+  textheading: { fontSize: 20, textAlign: 'justify' },
+  textlink: { color: 'red', textDecorationLine: 'underline' },
+});
