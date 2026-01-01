577e88104a206886d5a84676315bbaff2ed61132
Mansoor Ali <mansoorsahito601@gmail.com>
2026-01-01 09:45:42 +0500

    platform specific code

--- App.tsx
+++ App.tsx
@@ -1,38 +1,19 @@
-import { Alert, Image, StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, View, Platform } from 'react-native'
+import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Title</Text>
-      <Text style={styles.textheading} numberOfLines={4}>
-        Lorem Ipsum dolor sit
-        <Text
-          style={styles.textlink}
-          onPress={() => Alert.alert('Button Pressed ')}
-        >
-          Press Here
-        </Text>
-        amet, consectetur adisipiscing elit, sed do eiusmod tempor incididunt ut
-        lahore labore et dolore magna aliqua.
-      </Text>
-      <Image  source={require("./src/download.png")} style={styles.image}/>
-    </SafeAreaView>
-  );
-};
+      <Text> SO {Platform.OS==="android"?"This is Android Device":"This is Ios Device "}</Text>
+    </SafeAreaView >
+  )
+}
 
 export default App;
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: '#fff' },
-  text: {
-    textAlign: 'center',
-    fontSize: 50,
-    fontWeight: 'bold',
-    color: '#1b241f',
+  container:{flex:1,backgroundColor:Platform.OS==="android"?"#4d58d1ff":"#0000",
+    color:Platform.OS==="android"?"#fff":"#0000"
   },
-  textheading: { fontSize: 20, textAlign: 'justify' },
-  textlink: { color: 'red', textDecorationLine: 'underline' },
-  image:{height:100,width:100,borderRadius:20,marginTop:20,alignSelf:"center"}
-});
+})
