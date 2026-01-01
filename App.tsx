8356148fef2accbbf1ad90dc2fdf7c634d672951
Mansoor Ali <mansoorsahito601@gmail.com>
2026-01-01 09:53:25 +0500

    Loader implement

--- App.tsx
+++ App.tsx
@@ -1,19 +1,28 @@
-import { StyleSheet, Text, View, Platform } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text> SO {Platform.OS==="android"?"This is Android Device":"This is Ios Device "}</Text>
-    </SafeAreaView >
-  )
-}
+      <Text style={{    color: Platform.OS === 'android' ? '#fff' : '#0000',
+}}>
+        {' '}
+        SO{' '}
+        {Platform.OS === 'android'
+          ? 'This is Android Device'
+          : 'This is Ios Device '}
+          
+      </Text>
+    </SafeAreaView>
+  );
+};
 
 export default App;
 
 const styles = StyleSheet.create({
-  container:{flex:1,backgroundColor:Platform.OS==="android"?"#4d58d1ff":"#0000",
-    color:Platform.OS==="android"?"#fff":"#0000"
+  container: {
+    flex: 1,
+    backgroundColor: Platform.OS === 'android' ? '#4d58d1ff' : '#0000',
   },
-})
+});
