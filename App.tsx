d4d40f7d1ce17657f0ef09d5e43dd185ffcb4de9
Mansoor Ali <mansoorsahito601@gmail.com>
2026-01-02 10:17:27 +0500

    Flex wrap with direction

--- App.tsx
+++ App.tsx
@@ -1,18 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 const App = () => {
   return (
-    <View style={{backgroundColor:"tomato",flex:1}}>
-      <View style={{flex:1,backgroundColor:"black"}}/>
-            <View style={{flex:1,backgroundColor:"salmon"}}/>
-      <View style={{flex:1,backgroundColor:"blue"}}/>
+    <View
+      style={{
+        backgroundColor: 'white',
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
 
-
+        flexWrap: 'wrap',
+        alignContent: 'center',
+      }}
+    >
+      <View style={{ backgroundColor: 'black', width: 100, height: 100 }} />
+      <View style={{ backgroundColor: 'salmon', width: 100, height: 100 }} />
+      <View style={{ backgroundColor: 'blue', width: 100, height: 100 }} />
+      <View style={{ backgroundColor: 'red', width: 100, height: 100 }} />
     </View>
-  )
-}
+  );
+};
 
 export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
