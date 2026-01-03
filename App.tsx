a54574e408e5c91bf896fae469f6ce07c1fd1fbe
Mansoor Ali <mansoorsahito601@gmail.com>
2026-01-03 09:32:46 +0500

    Percentage Method

--- App.tsx
+++ App.tsx
@@ -1,27 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
 const App = () => {
   return (
-    <View
-      style={{
-        backgroundColor: 'white',
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center',
+    <View style={{flex:1,backgroundColor:"white"}}>
+      {/* Device Height wtith Percentage Method */}
+      <View style={{height:"100%",width:"50%",backgroundColor:"black"}}>
+        {/* Now try Dimenstions */}
 
-        flexWrap: 'wrap',
-        alignContent: 'center',
-      }}
-    >
-      <View style={{ backgroundColor: 'black', width: 100, height: 100 }} />
-      <View style={{ backgroundColor: 'salmon', width: 100, height: 100 }} />
-      <View style={{ backgroundColor: 'blue', width: 100, height: 100 }} />
-      <View style={{ backgroundColor: 'red', width: 100, height: 100 }} />
+      </View>
     </View>
-  );
-};
+  )
+}
 
 export default App;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({})
