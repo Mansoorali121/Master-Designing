ad5893cce2d66ee0bb932835c261f9998d68f178
Mansoor Ali <mansoorsahito601@gmail.com>
2026-01-03 09:37:11 +0500

    Device dimensions

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Dimensions, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const App = () => {
+  const deviceheight = Dimensions.get("screen").height;
+  const devicewidth = Dimensions.get("screen").width;
   return (
     <View style={{flex:1,backgroundColor:"white"}}>
       {/* Device Height wtith Percentage Method */}
-      <View style={{height:"100%",width:"50%",backgroundColor:"black"}}>
+      <View style={{height:deviceheight/2,width:devicewidth/1,backgroundColor:"black"}}>
         {/* Now try Dimenstions */}
+
 
       </View>
     </View>
