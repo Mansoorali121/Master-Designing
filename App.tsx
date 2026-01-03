86fed47f4db70b7c356495866191776783184d85
Mansoor Ali <mansoorsahito601@gmail.com>
2026-01-03 11:26:29 +0500

    imgbackground

--- App.tsx
+++ App.tsx
@@ -1,22 +1,17 @@
-import { StyleSheet, Text, View, Modal, Button } from 'react-native';
-import React, { useState } from 'react';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 const App = () => {
-  const [modal, setmodal] = useState(false);
-
   return (
-    <View
-      style={{ backgroundColor: 'gold', flex: 1, justifyContent: 'center' }}
+    <ImageBackground
+      source={{uri:"https://pikaso.cdnpk.net/private/production/2923865287/FfhrahOfbvJN3ePoc3ASLY2Am34cNzkomaCwFmlb.jpg?token=exp=1767744000~hmac=ba2ca455c6bcd2d04de87490c52256957823482a6fbea15db7c7ba29ef1f2624&preview=true&size=420"}}
+      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
     >
-      <Button title="Show Modal" onPress={() => setmodal(true)} />
+      <View style={{ backgroundColor: 'white', height: 300, width: 300 , borderRadius:20}}>
 
-      <Modal visible={modal} animationType="fade" transparent={true}>
-        <View style={{ flex:1, justifyContent:'center', alignItems:'center' }}>
-          <Text>Modal is Opened</Text>
-          <Button title="Close" onPress={() => setmodal(false)} />
-        </View>
-      </Modal>
-    </View>
+        <Text style={{textAlign:"center",top:20}}> Hello World</Text>
+      </View>
+    </ImageBackground>
   );
 };
 
