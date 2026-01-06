d69feee76c3f1afae3866a4114c51bee27cbca03
Mansoor Ali <mansoorsahito601@gmail.com>
2026-01-06 10:55:54 +0500

    Active Tab Add

--- src/components/Toptabs.js
+++ src/components/Toptabs.js
@@ -1,25 +1,54 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { s } from 'react-native-size-matters';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
+import { s, vs } from 'react-native-size-matters';
 
 const Toptabs = () => {
-    //  Tabs Array
-    const Tabsarray = ["Live","Recorded"]
-    // Tabs Active Color
-    const Active_Bg = "#75563B"
-    //  Active Text
-    const Active_Text = "#FFFFF"
-    //  InActive Text
-    const InActive_Text = "#2C2016"
+  //  Tabs Array
+  const Tabsarray = ['Live', 'Recorded'];
+  // Tabs Active Color
+  const Active_Bg = '#75563B';
+  //  Active Text
+  const Active_Text = '#FFFFF';
+  //  InActive Text
+  const InActive_Text = '#2C2016';
+
+  const [activetab, setActivetab] = useState('Recorded');
+
   return (
     <View style={styles.container}>
-      <Text>Toptabs</Text>
+      {Tabsarray.map(tabname => {
+        return (
+          <TouchableOpacity
+            style={[
+              styles.tabs,
+              activetab === tabname && { backgroundColor: Active_Bg },
+            ]}
+          >
+            <Text >{tabname}</Text>
+          </TouchableOpacity>
+        );
+      })}
     </View>
-  )
-}
+  );
+};
 
 export default Toptabs;
 
 const styles = StyleSheet.create({
-    container:{backgroundColor:"red",borderRadius:s(12)},
-})
+  container: {
+    borderRadius: s(12),
+    flexDirection: 'row',
+    backgroundColor: '#F5F5F4',
+    height: vs(48),
+    alignItems: 'center',
+    padding: s(4),
+  },
+  tabs: {
+    // backgroundColor: 'red',
+    height: vs(32),
+    flex: 1,
+    borderRadius: s(8),
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
